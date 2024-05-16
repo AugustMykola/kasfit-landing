e2decfe416ac24ef65a5759164fdc2ad3f13927e
Mykola Nosachenko <19hagiri96@gmail.com>
2024-05-17 02:03:57 +0300

    minor

--- src/app/core/providers/data-controller.service.ts
+++ src/app/core/providers/data-controller.service.ts
@@ -1,15 +1,70 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase} from "@angular/fire/compat/database";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataControllerService {
-  private path: string = '/data'
+  private path: string = '/data';
+  private pathContact: string = '/data/contactItemsList';
+  private pathLinks: string = '/data/linkList';
+  private pathServices: string = '/data/serviceItemListData';
+  private pathReviews: string = '/data/reviewItemListData';
+  private pathSocialMedia: string = '/data/socialMediaItemsList';
+  private pathBenefitsList: string = '/data/benefitsList';
+
+  private pathAboutMeSectionHeader: string = '/data/aboutMeSectionHeader';
+  private pathAboutMeSectionDescription: string = '/data/aboutMeSectionDescription';
+  private pathJoinButtonLabel: string = '/data/joinButtonLabel';
+  private pathReviewsSectionHeader: string = '/data/reviewsSectionHeader';
+  private pathServicesSectionHeader: string = '/data/servicesSectionHeader';
+
+
   constructor(private db: AngularFireDatabase) {
   }
 
-  getData() {
-    return this.db.list(this.path).valueChanges();
+  getDataContactItemsList() {
+    return this.db.list(this.pathContact).valueChanges();
+  }
+
+  getDataLinksList(): Observable<any> {
+    return this.db.list(this.pathLinks).valueChanges();
+  }
+
+  getDataServicesList(): Observable<any> {
+    return this.db.list(this.pathServices).valueChanges();
+  }
+
+  getDataReviewsList(): Observable<any> {
+    return this.db.list(this.pathReviews).valueChanges();
+  }
+
+  getDataSocialMediaList(): Observable<any> {
+    return this.db.list(this.pathSocialMedia).valueChanges();
+  }
+
+  getDataAboutMeSectionHeader(): Observable<any> {
+    return this.db.list(this.pathAboutMeSectionHeader).valueChanges();
+  }
+
+  getDataAboutMeSectionDescription(): Observable<any> {
+    return this.db.list(this.pathAboutMeSectionDescription).valueChanges();
+  }
+
+  getDataLoinButtonLabel(): Observable<any> {
+    return this.db.list(this.pathJoinButtonLabel).valueChanges();
+  }
+
+  getDataReviewsSectionHeader(): Observable<any> {
+    return this.db.list(this.pathReviewsSectionHeader).valueChanges();
+  }
+
+  getDataServicesSectionHeader(): Observable<any> {
+    return this.db.list(this.pathServicesSectionHeader).valueChanges();
+  }
+
+  getDataBenefitsList(): Observable<any> {
+    return this.db.list(this.pathBenefitsList).valueChanges();
   }
 }
